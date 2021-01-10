a62356688b3cc849c7b8a9bfd844339f4868ee38
annawang1109 <wang.ci.ci@163.com>
2021-01-10 14:02:38 +0800

    ok

--- socket/s01.py
+++ socket/s01.py
@@ -1,1 +1,21 @@
 import socket
+
+####
+#服务器端的本质
+####
+sock = socket.socket()
+sock.bind(('127.0.0.1', 8080))
+sock.listen(5)
+
+
+while True:
+    conn,addr = sock.accept()
+    # 有人来链接，获取用户发送的数据
+    data = conn.recv(8096)
+    print(data)
+    print(addr)
+
+    data.decode(uni.split("\r\n\r\n")
+    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.send(b"123123<br>123123")
+    conn.close()
---
+++ socket/s02_str_bytes转化.py
@@ -0,0 +1,8 @@
+
+# byte to str
+data_byte = b'abc'
+print(str(data_byte,encoding="utf-8"))
+
+# str to byte
+data_str = "abc"
+print(bytes(data_str, encoding="utf-8"))
