3511e00d2b80fad2dc59dcd10c8283bf2f1e3899
annawang1109 <wang.ci.ci@163.com>
2021-01-24 15:59:28 +0800

    ok

--- socket/s02_str_bytes转化.py
+++ socket/s02_str_bytes转化.py
@@ -6,3 +6,13 @@ print(str(data_byte,encoding="utf-8"))
 # str to byte
 data_str = "abc"
 print(bytes(data_str, encoding="utf-8"))
+
+def sum(x, y):
+    return x+y
+
+def multi(x, y):
+    return x*y
+
+data_set = [sum, multi]
+
+print(data_set[0](2,3))
